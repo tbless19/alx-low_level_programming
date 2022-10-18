1de233cabb755aadd87ef130ab6e32315ac5e39f
Prince Tawiah <ptawiah.coe@gmail.com>
2022-10-18 16:10:39 -0700

    Task 1 commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - print the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char pt;
+
+	for (pt = 'a'; pt <= 'z'; pt++)
+		_putchar(pt);
+
+	_putchar('\n');
+}
