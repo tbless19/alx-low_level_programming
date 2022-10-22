58e7bd658f26ad0a2c0f51bff5a3947a5277d63b
Prince Tawiah <ptawiah.coe@gmail.com>
2022-10-21 18:07:26 -0700

    Task 3 commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	for (int a = 0; a < 10; a++)
+		_putchar(a);
+
+	_putchat('\n');
+}
