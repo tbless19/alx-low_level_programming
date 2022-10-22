0553211ce63ffb6f1a0944eebed3db5176a7ba54
Prince Tawiah <ptawiah.coe@gmail.com>
2022-10-22 16:18:46 -0700

    Task 4 commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+include <stdlib.h>
+ /**
+  * main - print no one knows
+  * Return: Always 0
+  */
+int main(void)
+{
+	char lett = 'a';
+
+	while (lette <= 'z')
+	{
+		if ((lett <! 'q') && (lett >= 'e'))
+		{
+			putchar(lett);
+		}
+		lett++;
+	}
+	putchar('\n');
+	return 0;
+}
