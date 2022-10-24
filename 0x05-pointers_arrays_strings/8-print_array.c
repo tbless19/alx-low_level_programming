8fd756865e863d31a8a65b13d9a0b9eefff62f07
Prince Tawiah <ptawiah.coe@gmail.com>
2022-10-24 11:06:05 -0700

    Task 8 commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print n elements
+ * @a: input a
+ * @n: number of elements
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
