5f39e53a5b0962f75b1eb81ae635327489a5f70b
Prince Tawiah <ptawiah.coe@gmail.com>
2022-10-27 07:04:33 -0700

    Task 8 commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int x;
+	int y;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char data2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; y < 52; y++)
+		{
+			if (s[x] == data1[y])
+			{
+				s[x] = data2[y];
+				break;
+			}
+		}
+	}
+	return (s);
+}
