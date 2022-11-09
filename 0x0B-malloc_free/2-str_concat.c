5bccfbfa3437b47b22ba5ce78a617120f315994b
Prince Tawiah <ptawiah.coe@gmail.com>
2022-11-08 20:57:14 -0800

    Task 2 commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,9 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * _strlen - counts and returns string length
- * @s: that's the string
+ * @s: string
  * Return: length
  */
 int _strlen(char *s)
@@ -16,4 +18,37 @@ int _strlen(char *s)
 		s++;
 	}
 	return (counter);
+}
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: secnd string
+ * Return: new
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *new;
+	unsigned int i;
+	unsigned int j;
+	int total = 0;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	total += _strlen(s1) + _strlen(s2);
+	new = malloc((total * sizeof(char)) + 1);
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; s1[i]; i++)
+	{
+		new[i] = s1[i];
+	}
+	for (j = 0; s2[j]; j++, i++)
+	{
+		new[i] = s2[j];
+	}
+	new[i] = '\0';
+	return (new);
 }
